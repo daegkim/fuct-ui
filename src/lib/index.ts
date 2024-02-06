1a5e421b492c5f0101df96ee1803e8c7dd3ee32c
daegeun <daegeunkim92@gmail.com>
2024-02-06 21:17:22 +0900

    export에 추가

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,6 @@ export { ToastContext, useToast } from './components/toast';
 export { default as FuctToast, useFuctToast } from './components/toast/FuctToast';
 
 export { useModal, useModalComponent } from './components/modal';
+export { default as FuctConfirm } from './components/modal/FuctConfirm';
+
+export { default as Backdrop } from './components/backdrop';
